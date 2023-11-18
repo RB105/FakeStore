db16c056377343a9fc9e0e352bbb82eaff4a1742
RB105 <rb105@RB-Macbook.home>
2023-11-18 21:07:12 +0500

    ui updated

--- lib/view/screens/admin_panel.dart
+++ lib/view/screens/admin_panel.dart
@@ -1,6 +1,8 @@
 import 'package:fakestore/bloc/admin/admin_states.dart';
+import 'package:fakestore/core/extensions/build_context_ext.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shimmer/shimmer.dart';
 
 class AdminPanelPage extends StatefulWidget {
   const AdminPanelPage({super.key});
@@ -20,8 +22,22 @@ class _AdminPanelPageState extends State<AdminPanelPage> {
         body: Builder(
           builder: (context) {
             if (state is AdminLoadingState) {
-              return const Center(
-                child: CircularProgressIndicator(),
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                  itemBuilder: (context, index) => Shimmer.fromColors(
+                    baseColor: Colors.grey.shade400,
+                    highlightColor: Colors.white10,
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      height: context.height * 0.05,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.black),
+                    ),
+                  ),
+                ),
               );
             } else if (state is AdminErrorState) {
               return Center(
@@ -57,7 +73,8 @@ class _AdminPanelPageState extends State<AdminPanelPage> {
             ? IconButton(
                 onPressed: () async {
                   await context.read<AdminCubit>().getAllCarts();
-                }, icon: const Icon(Icons.refresh))
+                },
+                icon: const Icon(Icons.refresh))
             : const SizedBox(),
       ),
     );
